ba81421647bf62273b7a819ad5103bb6d47d93c2
Ria-ag <144191972+Ria-ag@users.noreply.github.com>
2024-06-25 00:16:21 -0700

    manifest update

--- android/app/src/main/kotlin/com/example/mobileapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mobileapp/MainActivity.kt
@@ -1,5 +1,13 @@
-package com.example.mobileapp
+package com.example.rise;
 
-import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent;
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
 
-class MainActivity: FlutterActivity()
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+}
